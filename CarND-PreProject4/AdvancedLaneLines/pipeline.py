3855d6d5f5d4a1f5e26488215e102ebaad2958e1
Saajan Shridhar <saajan.is@gmail.com>
2017-08-03 18:54:13 -0700

    P4

--- CarND-PreProject4/AdvancedLaneLines/pipeline.py
+++ CarND-PreProject4/AdvancedLaneLines/pipeline.py
@@ -1,7 +1,13 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 import math
 from os import listdir
 import matplotlib.pyplot as plt
-from tensorflow.python.summary.summary import image
+plt.ion()
 
 def load_images(IMG_DIR):
     images = []
@@ -13,21 +19,24 @@ def load_images(IMG_DIR):
     return images
 
 def show_images(images, make_random=False, fig_title='Default title', CMAP=None):
-    pass
-#     count = len(images)
-#     col_count = 5
-#     rows = math.ceil(float(len(images)) / float(col_count))
-#     fig = plt.figure(figsize=(4*int(col_count),2*rows))
-#     fig.suptitle(fig_title, fontsize=16)
-#     
-#     for i in range(count):
-#         image = images[i]
-#         ax1 = fig.add_subplot(rows,col_count,i+1)
-#         #ax1.set_title(labels[i] + ' occurences', fontsize=8)
-#         ax1.imshow(image, cmap=CMAP)
+    count = len(images)
+    col_count = 5
+    rows = math.ceil(float(len(images)) / float(col_count))
+    fig = plt.figure(figsize=(4*int(col_count),2*rows))
+    fig.suptitle(fig_title, fontsize=16)
+    
+    for i in range(count):
+        image = images[i]
+        ax1 = fig.add_subplot(rows,col_count,i+1)
+        #ax1.set_title(labelssamples[i] + ' occurences', fontsize=8)
+        ax1.imshow(image, cmap=CMAP)
+        #cv2.imwrite("./samples/" + str(fig_title) + "_" + str(i) + ".png", image)
     #plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-    
-    
+
+
+# In[2]:
+
+
 import cv2
 import numpy as np
 
@@ -50,22 +59,30 @@ def calibrate_camera(images):
     
     return (mtx, dist)
 
-def undistort_image(image, mtx, dist):
+def undistort_image(image, mtx, dist, original_image = None):
     return cv2.undistort(image, mtx, dist, None, mtx)
 
 calibration_images = load_images('./camera_cal/')
-#show_images(calibration_images, fig_title='Original Calibration Images')
+show_images(calibration_images, fig_title='Original Calibration Images')
 
+undistorted_images = []
 (MTX, DIST) = calibrate_camera(calibration_images)
-#show_images(undistorted_images, fig_title='Undistorted Images')
+for image in calibration_images:
+    undistorted_images.append(undistort_image(image, MTX, DIST))
+show_images(undistorted_images, fig_title='Undistorted Images')
+
+
+# In[36]:
+
 
 # Transform color and gradient
 # TODO(saajan): See if this needs improvement
-def transform_sobel_s_(image, s_thresh=(170, 255), sx_thresh=(20, 100)):
+def transform_sobel_s_r(image, s_thresh=(90, 255), r_thresh =(200, 255), sx_thresh=(15, 100), original_image = None):
     image = np.copy(image)
     
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HLS).astype(np.float)
+    r_channel = image[:,:,0]
     s_channel = hsv[:,:,2]
     
     # Sobel x
@@ -77,14 +94,22 @@ def transform_sobel_s_(image, s_thresh=(170, 255), sx_thresh=(20, 100)):
     sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
     
     # Threshold color channel
+    # Threshold color channel
+    r_binary = np.zeros_like(r_channel)
+    r_binary[(r_channel >= r_thresh[0]) & (r_channel <= r_thresh[1])] = 1
+    ##
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
     
     # Combine the two binary thresholds
     combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    combined_binary[(s_binary == 1) | (sxbinary == 1) | (r_binary == 1)] = 1
     
     return combined_binary
+
+
+# In[37]:
+
 
 # Perspective transform
 
@@ -92,7 +117,7 @@ SRC = np.float32([[590, 460], [330, 670], [1060, 670], [700, 460]])
 DST = np.float32([[330, 0], [330, 670], [1060, 670], [1060, 0]])
 M = cv2.getPerspectiveTransform(SRC, DST)
 M_inverse = cv2.getPerspectiveTransform(DST, SRC)
-def transform_perspective(image):
+def transform_perspective(image, original_image = None):
     [y_orig, x_orig] = image.shape[:2]
     warped = cv2.warpPerspective(image, M, (x_orig, y_orig), flags=cv2.INTER_LINEAR)
 #     # Testing by drawing a rectangle
@@ -104,6 +129,10 @@ def transform_perspective_reverse(image, original_image = None):
     [y_orig, x_orig] = image.shape[:2]
     warped = cv2.warpPerspective(image, M_inverse, (x_orig, y_orig), flags=cv2.INTER_LINEAR)
     return warped
+
+
+# In[50]:
+
 
 from moviepy.video.io.bindings import mplfig_to_npimage
 
@@ -150,11 +179,26 @@ def find_window_centroids(image, window_width, window_height, margin):
 
     return window_centroids
 
+NUM_FRAMES_CONSIDERED = 20
+LEFT_FIT_POINTS = []
+RIGHT_FIT_POINTS = []
 LEFT_FIT = None
 RIGHT_FIT = None
+LEFT_FIT_CR = None
+RIGHT_FIT_CR = None
+# Define conversions in x and y from pixels space to meters
+ym_per_pix = 30/720 # meters per pixel in y dimension
+xm_per_pix = 3.7/700 # meters per pixel in x dimension
 def locate_lane_lines(image, original_image = None):
     global LEFT_FIT
     global RIGHT_FIT
+    global ym_per_pix
+    global xm_per_pix
+    global LEFT_FIT_CR
+    global RIGHT_FIT_CR
+    global NUM_FRAMES_CONSIDERED
+    global LEFT_FIT_POINTS
+    global RIGHT_FIT_POINTS
     
     out_img = np.dstack((image, image, image))*255
     window_img = np.zeros_like(out_img)
@@ -167,104 +211,183 @@ def locate_lane_lines(image, original_image = None):
     nonzero_img_mask[nonzeroy, nonzerox] = 1
 
     # Set the width of the windows +/- margin
-    margin = 150
+    margin = 175
     # Set minimum number of pixels required to recenter window
     minpix = 50
     # Set minimum number of pixels required to reconsider last image's fits
-    # TODO(saajan): Rethink the value of this param
-    min_pix_full_image = 50000
     
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
     
-    # Evaluate if the last fit can be used
-    if(LEFT_FIT is not None and RIGHT_FIT is not None):
-        left_lane_inds_temp = ((nonzerox > (LEFT_FIT[0]*(nonzeroy**2) + LEFT_FIT[1]*nonzeroy + LEFT_FIT[2] - margin)) & (nonzerox < (LEFT_FIT[0]*(nonzeroy**2) + LEFT_FIT[1]*nonzeroy + LEFT_FIT[2] + margin))) 
-        right_lane_inds_temp = ((nonzerox > (RIGHT_FIT[0]*(nonzeroy**2) + RIGHT_FIT[1]*nonzeroy + RIGHT_FIT[2] - margin)) & (nonzerox < (RIGHT_FIT[0]*(nonzeroy**2) + RIGHT_FIT[1]*nonzeroy + RIGHT_FIT[2] + margin)))  
-        # Again, extract left and right line pixel positions
-        leftx_temp = nonzerox[left_lane_inds_temp]
-        rightx_temp = nonzerox[right_lane_inds_temp]
-        
-        if (len(leftx_temp) + len(rightx_temp) < min_pix_full_image):
-            LEFT_FIT = None
-            RIGHT_FIT = None
-        
-    if(LEFT_FIT is None and RIGHT_FIT is None):
-        # window settings
-        window_width = 50 
-        window_height = 80 # Break image into 9 vertical layers since image height is 720
-        
-        window_centroids = find_window_centroids(image, window_width, window_height, margin)
-
-        left_img_mask = np.zeros_like(image)
-        right_img_mask = np.zeros_like(image)
-        # If we found any window centers
-        if len(window_centroids) > 0:
-            # Go through each level and draw the windows     
-            for level in range(0,len(window_centroids)):
-                # Window_mask is a function to draw window areas
-                l_mask = window_mask(window_width,window_height,image,window_centroids[level][0],level)
-                r_mask = window_mask(window_width,window_height,image,window_centroids[level][1],level)
+#     # Evaluate if the last fit can be used
+#     if(LEFT_FIT is not None and RIGHT_FIT is not None):
+#         left_lane_inds_temp = ((nonzerox > (LEFT_FIT[0]*(nonzeroy**2) + LEFT_FIT[1]*nonzeroy + LEFT_FIT[2] - margin)) & (nonzerox < (LEFT_FIT[0]*(nonzeroy**2) + LEFT_FIT[1]*nonzeroy + LEFT_FIT[2] + margin))) 
+#         right_lane_inds_temp = ((nonzerox > (RIGHT_FIT[0]*(nonzeroy**2) + RIGHT_FIT[1]*nonzeroy + RIGHT_FIT[2] - margin)) & (nonzerox < (RIGHT_FIT[0]*(nonzeroy**2) + RIGHT_FIT[1]*nonzeroy + RIGHT_FIT[2] + margin)))  
+#         # Again, extract left and right line pixel positions
+#         leftx_temp = nonzerox[left_lane_inds_temp]
+#         rightx_temp = nonzerox[right_lane_inds_temp]
                 
-                # Accumulate window areas
-                left_img_mask = left_img_mask | l_mask
-                right_img_mask = right_img_mask | r_mask
-         
-        # Final non-zero pixels in window areas    
-        left_img_mask = left_img_mask & nonzero_img_mask
-        right_img_mask = right_img_mask & nonzero_img_mask
+#         total_pix = len(leftx_temp) + len(rightx_temp)
+#         if (total_pix < 65000):
+#             LEFT_FIT = None
+#             RIGHT_FIT = None
+#             LEFT_FIT_CR = None
+#             RIGHT_FIT_CR = None
         
-        nonzero_left = left_img_mask.nonzero()
-        nonzero_right = right_img_mask.nonzero()
-        
-        # Fit a second order polynomial to each
-        LEFT_FIT = np.polyfit(nonzero_left[0], nonzero_left[1], 2)
-        RIGHT_FIT = np.polyfit(nonzero_right[0], nonzero_right[1], 2)
+#     if(LEFT_FIT is None or RIGHT_FIT is None):
+        # window settings
+    window_width = 50 
+    window_height = 80 # Break image into 9 vertical layers since image height is 720
+
+    window_centroids = find_window_centroids(image, window_width, window_height, margin)
+
+    left_img_mask = np.zeros_like(image)
+    right_img_mask = np.zeros_like(image)
+    # If we found any window centers
+    if len(window_centroids) > 0:
+        # Go through each level and draw the windows     
+        for level in range(0,len(window_centroids)):
+            # Window_mask is a function to draw window areas
+            l_mask = window_mask(window_width,window_height,image,window_centroids[level][0],level)
+            r_mask = window_mask(window_width,window_height,image,window_centroids[level][1],level)
+
+            # Accumulate window areas
+            left_img_mask = left_img_mask | l_mask
+            right_img_mask = right_img_mask | r_mask
+
+    # Final non-zero pixels in window areas    
+    left_img_mask = left_img_mask & nonzero_img_mask
+    right_img_mask = right_img_mask & nonzero_img_mask
+
+    nonzero_left = left_img_mask.nonzero()
+    nonzero_right = right_img_mask.nonzero()
+    
+    #Add to list
+    LEFT_FIT_POINTS = [nonzero_left] + LEFT_FIT_POINTS[:NUM_FRAMES_CONSIDERED-1]
+    RIGHT_FIT_POINTS = [nonzero_right] + RIGHT_FIT_POINTS[:NUM_FRAMES_CONSIDERED-1]
+    
+    # Fit a second order polynomial to each
+    left_fit_x = np.concatenate([points[0] for points in LEFT_FIT_POINTS])
+    left_fit_y = np.concatenate([points[1] for points in LEFT_FIT_POINTS])
+    right_fit_x = np.concatenate([points[0] for points in RIGHT_FIT_POINTS])
+    right_fit_y = np.concatenate([points[1] for points in RIGHT_FIT_POINTS])
+    LEFT_FIT = np.polyfit(
+        left_fit_x, 
+        left_fit_y, 
+        2)
+    RIGHT_FIT = np.polyfit(
+        right_fit_x,
+        right_fit_y, 
+        2)
+
+    LEFT_FIT_CR = np.polyfit(left_fit_x*ym_per_pix, left_fit_y*xm_per_pix, 2)
+    RIGHT_FIT_CR = np.polyfit(right_fit_x*ym_per_pix, right_fit_y*xm_per_pix, 2)
             
-    if(LEFT_FIT is None and RIGHT_FIT is None):
-        return image
-    else:
-        # Visualizing
-        # Generate x and y values for plotting
-        ploty = np.linspace(0, image.shape[0]-1, image.shape[0] )
-        left_fitx = LEFT_FIT[0]*ploty**2 + LEFT_FIT[1]*ploty + LEFT_FIT[2]
-        right_fitx = RIGHT_FIT[0]*ploty**2 + RIGHT_FIT[1]*ploty + RIGHT_FIT[2]
-        # Generate a polygon to illustrate the search window area
-        # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
-        right_line_pts = np.hstack((right_line_window1, right_line_window2))
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
-        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-        
-        reverse_transformed_mask = transform_perspective_reverse(window_img)
-        #reverse_transformed_mask = np.dstack((reverse_transformed_mask, reverse_transformed_mask, reverse_transformed_mask))*255
-        
-        result = cv2.addWeighted(original_image, 1, reverse_transformed_mask, 0.3, 0) 
-        
-        return result
+#     if(LEFT_FIT is None or RIGHT_FIT is None):
+#         return image
+#     else:
+    # Visualizing
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, image.shape[0]-1, image.shape[0] )
+    y_eval = np.max(ploty)
+    left_fitx = LEFT_FIT[0]*ploty**2 + LEFT_FIT[1]*ploty + LEFT_FIT[2]
+    right_fitx = RIGHT_FIT[0]*ploty**2 + RIGHT_FIT[1]*ploty + RIGHT_FIT[2]
+
+    points_left = np.array([[[xi, yi]] for xi, yi in zip(left_fitx, ploty)]).astype(np.int32)
+    points_right = np.array([[[xi, yi]] for xi, yi in zip(right_fitx, ploty)]).astype(np.int32)
+    points_right = np.flipud(points_right)
+    lane_fill_points = np.concatenate((points_left, points_right))
+    cv2.fillPoly(window_img, [lane_fill_points], (0,255, 0))
+
+    # Calculate vehicle offset
+    centre_of_image = image.shape[1]/2
+    centre_of_car = left_fitx[len(left_fitx)-1] + np.abs(right_fitx[len(right_fitx)-1] - left_fitx[len(left_fitx)-1])/2.0;
+    offset = (centre_of_image - centre_of_car) * xm_per_pix
+    offset_str = 'Vehicle is ' + str("{0:.2f}".format(np.abs(offset))) + ' m ' + str('left of center' if (offset < 0.0) else 'right of center')
+
+    # Reverse perspective
+    reverse_transformed_mask = transform_perspective_reverse(window_img)
+    #reverse_transformed_mask = np.dstack((reverse_transformed_mask, reverse_transformed_mask, reverse_transformed_mask))*255
+    result = cv2.addWeighted(original_image, 1, reverse_transformed_mask, 0.3, 0) 
+
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*LEFT_FIT_CR[0]*y_eval*ym_per_pix + LEFT_FIT_CR[1])**2)**1.5) / np.absolute(2*LEFT_FIT_CR[0])
+    right_curverad = ((1 + (2*RIGHT_FIT_CR[0]*y_eval*ym_per_pix + RIGHT_FIT_CR[1])**2)**1.5) / np.absolute(2*RIGHT_FIT_CR[0])
+    # Now our radius of curvature is in meters
+    #print(left_curverad, 'm', right_curverad, 'm')
+    # Example values: 632.1 m    626.2 m
+
+    #cv2.putText(result,"Radius of curvature: " + left_curverad + " | " + right_curverad, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+    cv2.putText(result, "Radius of curvature: " + str("{0:.2f}".format(left_curverad)) + "m | "        + str("{0:.2f}".format(right_curverad)) + "m", (int(50), int(50)), cv2.FONT_HERSHEY_PLAIN,        3, (255,255,255), 7, cv2.LINE_AA)
+        #, size, color, thickness)
+    cv2.putText(result, offset_str, (int(50), int(100)), cv2.FONT_HERSHEY_PLAIN,        3, (255,255,255), 7, cv2.LINE_AA)
 
 
+    return result
 
 
-####################################
+# In[51]:
+
+
 from functools import partial
 
 #Pipeline
 test_images = load_images('./test_images/')
 
-PIPELINE_FUNCS = [partial(undistort_image, mtx = MTX, dist = DIST), transform_sobel_s_, transform_perspective,
+PIPELINE_FUNCS = [partial(undistort_image, mtx = MTX, dist = DIST), transform_sobel_s_r, transform_perspective,
                  locate_lane_lines]
 
-transformed_images = test_images
+transformed_images = [(image, image) for image in test_images]
 for func in PIPELINE_FUNCS:
-    transformed_images = [func(image) for image in transformed_images]
+    transformed_images = [(func(image_tuple[0], original_image = image_tuple[1]), image_tuple[1]) for image_tuple in transformed_images]
 
-show_images(transformed_images, fig_title='Transformed Images')
+    show_images([image_tuple[0] for image_tuple in transformed_images], fig_title='Transformed Images', CMAP='gray')
+
+
+# In[ ]:
+
+
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+
+def process_image(image):
+    # NOTE: The output you return should be a color image (3 channel) for processing video below
+    # TODO: put your pipeline here,
+    # you should return the final output (image where lines are drawn on lanes)
+    
+    #Pipeline
+    PIPELINE_FUNCS = [partial(undistort_image, mtx = MTX, dist = DIST), transform_sobel_s_r, transform_perspective,
+                     locate_lane_lines]
+    transformed_image = image
+    for func in PIPELINE_FUNCS:
+        transformed_image = func(transformed_image, original_image = image)
+    return transformed_image
+
+output_path = 'output_video.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+project_video = VideoFileClip("project_video.mp4")#.subclip(0,5)
+project_clip = project_video.fl_image(process_image) #NOTE: this function expects color images!!
+get_ipython().magic('time project_clip.write_videofile(output_path, audio=False)')
+
+
+# In[10]:
+
+
+HTML("""
+<video width="960" height="540" controls>
+  <source src="{0}">
+</video>
+""".format(output_path))
+
+
+# In[ ]:
+
+
+
+
